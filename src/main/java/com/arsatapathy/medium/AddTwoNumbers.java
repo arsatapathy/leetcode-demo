83484d0ea24fc720683defe9d5b415b04e3a1165
arsatapathy <ashish.satapathy2014@gmail.com>
2022-01-24 15:45:53 +0530

    Add Two Numbers

--- src/main/java/com/arsatapathy/medium/AddTwoNumbers.java
+++ src/main/java/com/arsatapathy/medium/AddTwoNumbers.java
@@ -4,7 +4,7 @@ public class AddTwoNumbers {
 
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode sumNode = null;
-        ListNode newNode = null;
+        ListNode current = null;
 
         ListNode cur1 = l1;
         ListNode cur2 = l2;
@@ -12,91 +12,24 @@ public class AddTwoNumbers {
         int carry = 0;
         int sum = 0;
 
-        while (cur1 != null && cur2 != null) {
+        while (cur1 != null || cur2 != null) {
+            sum = ((cur1 != null) ? cur1.val : 0) + ((cur2 != null) ? cur2.val :0) + carry;
+            carry = sum / 10;
 
-            sum = cur1.val + cur2.val + carry;
-
-            if (sum >= 10) {
-                carry = sum / 10;
-                sum = sum - carry * 10;
-            } else
-                carry = 0;
-
-            newNode = new ListNode(sum);
-
-            if (sumNode == null) {
-                sumNode = newNode;
+            if (current == null) {
+                sumNode = new ListNode(sum % 10);
+                current = sumNode;
             } else {
-                ListNode current = sumNode;
-
-                while (current.next != null) current = current.next;
-
-                current.next = newNode;
+                current.next = new ListNode(sum %10);
+                current = current.next;
             }
 
-            cur1 = cur1.next;
-            cur2 = cur2.next;
+            if (cur1 != null) cur1 = cur1.next ;
+            if (cur2 != null) cur2 = cur2.next;
         }
 
-        while (cur1 != null) {
-
-            sum = cur1.val + carry;
-
-            if (sum >= 10) {
-                carry = sum / 10;
-                sum = sum - carry * 10;
-            } else
-                carry = 0;
-
-            newNode = new ListNode(sum);
-
-            if (sumNode == null) {
-                sumNode = newNode;
-            } else {
-                ListNode current = sumNode;
-
-                while (current.next != null) current = current.next;
-
-                current.next = newNode;
-            }
-
-            cur1 = cur1.next;
-        }
-
-        while (cur2 != null) {
-
-            sum = cur2.val + carry;
-
-            if (sum >= 10) {
-                carry = sum / 10;
-                sum = sum - carry * 10;
-            } else
-                carry = 0;
-
-            newNode = new ListNode(sum);
-
-            if (sumNode == null) {
-                sumNode = newNode;
-            } else {
-                ListNode current = sumNode;
-
-                while (current.next != null) current = current.next;
-
-                current.next = newNode;
-            }
-
-            cur2 = cur2.next;
-        }
-
-        if (carry != 0) {
-            newNode = new ListNode(carry);
-
-            ListNode current = sumNode;
-
-            while (current.next != null) current = current.next;
-
-            current.next = newNode;
-
+        if (carry > 0) {
+            current.next = new ListNode(carry);
         }
 
         return sumNode;
